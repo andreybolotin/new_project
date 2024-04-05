1952e0a16efc8f7007f9ffd920fca8171cda023c
andreybolotin <andrejbolotin138@gmail.com>
2024-04-06 00:55:27 +0300

    word

--- str_finc.py
+++ str_finc.py
@@ -2,6 +2,8 @@ new_word = input("Введите слово:")
 
 
 def word(new_world):
+    """Принимает строку и возвращает ее
+    со всеми заглавными буквами"""
     new_world = new_world.upper()
     return new_world
 

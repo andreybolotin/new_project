8cb2ddd0936aab8338e256d2f65c30bad88dc8ca
andreybolotin <165336268+andreybolotin@users.noreply.github.com>
2024-04-06 00:48:29 +0300

    str_finc.py

--- str_finc.py
+++ str_finc.py
@@ -2,8 +2,6 @@ new_word = input("Введите слово:")
 
 
 def word(new_world):
-    """Принимает строку и возвращает ее
-    в верхний регистр"""
     new_world = new_world.upper()
     return new_world
 

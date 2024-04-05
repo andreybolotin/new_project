3b2afbd7adb8d8d6c6b33a7659588f7cd714fd3e
andreybolotin <andrejbolotin138@gmail.com>
2024-04-06 00:23:21 +0300

    words

--- str_finc.py
+++ str_finc.py
@@ -9,3 +9,15 @@ def word(new_world):
 
 
 print(word(new_word))
+
+new_word = input("Введите слово:")
+
+
+def words(news_word):
+    """Принимает строку и возвращает
+    с первыми заглавными буквами"""
+    news_word = news_word.capitalize()
+    return news_word
+
+
+print(words(new_word))

b39297b6fc27934e40f9c44e6c6f73cc8050345d
Andrey Bolotin <165336268+andreybolotin@users.noreply.github.com>
2024-04-06 23:41:39 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -2,6 +2,8 @@ new_word = input("Введите слово: ")
 
 
 def word(new_word):
+    """Принимает строку и возвращает ее 
+    со всеми заглавными буквами"""
     new_word = new_word.upper()
     return new_word
 
